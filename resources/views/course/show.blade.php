0a877e35b5ff27c5de804226dd98cedff5d1da77
ashahir <ashahir@gmail.com>
2021-10-20 09:55:06 +0800

    content header

--- resources/views/course/show.blade.php
+++ resources/views/course/show.blade.php
@@ -1,22 +1,7 @@
 @extends('app')
 
 @section('content')
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>{{ $pageTitle }}</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">{{ $pageTitle }}</li>
-                    </ol>
-                </div>
-            </div>
-        </div><!-- /.container-fluid -->
-    </section>
+    @include('layouts.contentheader')
 
     <!-- Main content -->
     <section class="content">
